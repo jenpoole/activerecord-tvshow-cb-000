92903910e40c8d2b4957391da4c6715e3204a095
Jen Poole <contact.vandepool@gmail.com>
2018-02-20 02:40:15 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -35,5 +35,6 @@ class Show < ActiveRecord::Base
   # returns an array of all of the shows sorted by alphabetical order according to their names
   # hint: use the #order Active Record method
   def self.shows_by_alphabetical_order
+    Show.order("name ASC")
   end
 end
